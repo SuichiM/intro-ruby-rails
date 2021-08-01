7e17fda5ee91efde875985136e94f39461c8efee
SuichiM <MartiniRoqueEsteban@gmail.com>
2021-08-01 11:11:44 -0300

    creating an article and showed it

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -2,6 +2,17 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def show
+    
+    #  note the use of an instance variable 
+    #  (prefixed with @) to hold a reference to the article object. 
+    #  We do this because Rails will pass all instance variables to the view.
+    @article = Article.find(params[:id])
+  end
+ 
+  def new
+  end
+
   def create
     @article = Article.new(article_params)
  
